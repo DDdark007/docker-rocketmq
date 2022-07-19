aa24fa363b29fb4b2b4ab39c9a8e4e00b5f2b148
dakr <dakeke1123@gmail.com>
2022-07-19 20:54:27 +0800

    修改脚本

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 # 添加虚拟网卡
+rpm -q tunctl &>/dev/null
+if [[ $? -eq 0 ]];then
+	ip a | grep tap0 &>/dev/null
+	if [[ $? -eq 0 ]];then
+		echo "内网IP："$(ip a | grep tap0 | grep inet | awk '{print $2}' | awk -F '/' '{print $1}')
+	else
+		# 添加网卡 tap0 是虚拟网卡名字
+		tunctl -t tap0 -u root
+		ifconfig tap0 192.168.1.20 netmask 255.255.255.0 promisc
+	
+else
 cat << EOF >> /etc/yum.repos.d/nux-misc.repo
 [nux-misc]
 name=Nux Misc
@@ -13,14 +24,16 @@ yum --enablerepo=nux-misc install tunctl -y
 # 添加网卡 tap0 是虚拟网卡名字
 tunctl -t tap0 -u root
 ifconfig tap0 192.168.1.20 netmask 255.255.255.0 promisc
+fi
+
 # 创建目录
 mkdir -p ./broker/{conf,logs,store}
 mkdir -p ./namesrv/{logs,store}
 # 设置目录权限
-chmor chmod -R 777 ./broker/logs
-chmor chmod -R 777 ./broker/store
-chmor chmod -R 777 ./namesrv/logs
-chmor chmod -R 777 ./namesrv/store
+chmod -R 777 ./broker/logs
+chmod -R 777 ./broker/store
+chmod -R 777 ./namesrv/logs
+chmod -R 777 ./namesrv/store
 # 修改配置文件
 Ip=$(ip a | grep tap0 | grep inet | awk '{print $2}' | awk -F '/' '{print $1}')
 Ip2=$Ip":9876"
@@ -28,7 +41,6 @@ cat << EOF >> ./broker/conf/broker.conf
 brokerIP1=$Ip
 namesrvAddr=$Ip2
 EOF
-broker启动默认会取第一个网卡的IP作为监听IP，一般为内网IP。
 # 启动
 docker-compose up -d
 # 查看
