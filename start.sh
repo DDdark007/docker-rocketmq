36e272b5c7fae0a3988340082af46cffdacb6b44
dakr <dakeke1123@gmail.com>
2022-07-19 20:15:54 +0800

    初始化

---
+++ start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# 添加虚拟网卡
+cat << EOF >> /etc/yum.repos.d/nux-misc.repo
+[nux-misc]
+name=Nux Misc
+baseurl=http://li.nux.ro/download/nux/misc/el7/x86_64/
+enabled=0
+gpgcheck=1
+gpgkey=http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
+EOF
+# 安装命令
+yum --enablerepo=nux-misc install tunctl -y
+# 添加网卡 tap0 是虚拟网卡名字
+tunctl -t tap0 -u root
+ifconfig tap0 192.168.1.20 netmask 255.255.255.0 promisc
+# 创建目录
+mkdir -p /usr/local/rocketmq/broker/{conf,logs,store}
+mkdir -p /usr/local/rocketmq/namesrv/{logs,store}
+# 设置目录权限
+chmor chmod -R 777 /usr/local/rocketmq/broker/logs
+chmor chmod -R 777 /usr/local/rocketmq/broker/store
+chmor chmod -R 777 /usr/local/rocketmq/namesrv/logs
+chmor chmod -R 777 /usr/local/rocketmq/namesrv/store
+# 修改配置文件
+Ip=$(ip a | grep tap0 | grep inet | awk '{print $2}' | awk -F '/' '{print $1}')
+Ip2=$Ip":9876"
+cat << EOF >> /usr/local/rocketmq/broker/conf/broker.conf
+brokerIP1=$Ip
+namesrvAddr=$Ip2
+EOF
+broker启动默认会取第一个网卡的IP作为监听IP，一般为内网IP。
+# 启动
+docker-compose up -d
+# 查看
+docker ps |grep rocketmq
